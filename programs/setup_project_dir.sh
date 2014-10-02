cf24ad5083bf202e073c374199ab5b7d8faaf4e5
Ethan Durrant <emd519@yahoo.com>
2014-10-02 14:15:24 -0700

    added file overwrite protection to the script

--- programs/setup_project_dir.sh
+++ programs/setup_project_dir.sh
@@ -4,8 +4,13 @@ BUILD_THINGS_DIR="../build_things"
 
 PROJ_DIR=$1
 
-if [ "$1" != "" ]; then
-    echo "project dir name = $1"
+if [ "$PROJ_DIR" != "" ]; then
+        if [ -d $PROJ_DIR ]; then
+            echo "project dir $PROJ_DIR already exists! aborting script."
+            exit 1
+        else
+            echo "project dir name = $PROJ_DIR"
+        fi
 else
     echo "Please specify a project dir name"
     exit 1
