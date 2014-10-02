49a6cf3e0b89e347ef865945ca51de9a418f81f2
Ethan Durrant <emd519@yahoo.com>
2014-10-02 13:49:16 -0700

    may be done with basic script, but probably not.

--- programs/setup_project_dir.sh
+++ programs/setup_project_dir.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-BUILD_THINGS_DIR = ../build_things/
+BUILD_THINGS_DIR = ../build_things
 
+PROJ_DIR = $1
 
 if [ "$1" != "" ]; then
     echo "project dir name = $1"
 else
     echo "Please specify a project dir name"
 fi
+
+echo -n "making project dir $PROJ_DIR"
+mkdir PROJ_DIR
+
+echo -n "coping contents of $BUILD_THINGS_DIR to $PROJ_DIR"
+cp $BUILD_THINGS_DIR/* $PROJ_DIR
 
