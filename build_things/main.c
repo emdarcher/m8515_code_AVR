98ff692739fa253fddd5512457ba69e78496e738
Ethan Durrant <emd519@yahoo.com>
2014-10-02 13:34:19 -0700

    making a script for making project dirs

--- build_things/main.c
+++ build_things/main.c
@@ -2,7 +2,25 @@
 
 #include "main.h"
 
+/* defines */
+
+/* function prototypes */
+
 void main(void){
 
-
+    /* init stuff */
+    
+    /* infinite loop */
+    while(1){
+        
+    }
 }
+
+/* function bodies */
+
+
+
+/* Interrupt Service Routines */
+
+
+
---
+++ programs/setup_project_dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUILD_THINGS_DIR = ../build_things/
+
+
+if [ "$1" != "" ]; then
+    echo "project dir name = $1"
+else
+    echo "Please specify a project dir name"
+fi
+
